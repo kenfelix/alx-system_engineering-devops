ac19c13ae17e4662f96b1d98bac9d671d53d6331
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-23 00:50:20 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # Installs flask
+include python
+include python::flask
 package { 'Flask 2.1.0':
   ensure   => '2.1.0',
-  name     => 'Flask 2.1.0',
+  name     => 'flask',
   provider => 'pip3'
+  require  => Class['python::flask'],
 }
