965b12c12562f665c89a41f2c69f5cfe4a413a98
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-23 00:24:28 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip'
-}
+ # Installs flask
+ exec { 'install python packages':
+     command   => 'pip3 install flask flask_restful apiai',
+     path => ['/usr/bin/'],
+     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  }
