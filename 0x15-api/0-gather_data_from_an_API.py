f7872dac663a28773857ff7fb3ac5c85a731e7e1
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-16 10:43:44 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@
 import requests
 from sys import argv
 
-if __name__ == '__main__':
+def main():
     endpoint = "https://jsonplaceholder.typicode.com/"
     userId = argv[1]
     user = requests.get(endpoint + "users/{}".
@@ -19,3 +19,6 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".
           format(user.get('name'), len(completed_tasks), len(todo)))
     print("\n".join("\t {}".format(task) for task in completed_tasks))
+
+if __name__ == '__main__':
+    main()
