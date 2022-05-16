531766ae8ae351bd67dcd2a3e6b11b31202c3638
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-16 10:44:37 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ import requests
 from sys import argv
 
 def main():
+    """Main Function"""
     endpoint = "https://jsonplaceholder.typicode.com/"
     userId = argv[1]
     user = requests.get(endpoint + "users/{}".
