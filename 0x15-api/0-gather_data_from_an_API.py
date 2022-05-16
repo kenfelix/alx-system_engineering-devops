1658cb2d61f9c82a9f6754c15d0694e0529dda09
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-16 10:35:57 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,17 @@
-"""Getting Data from an API"""
+#!/usr/bin/python3
+"""getting data from an api
+"""
 
 import requests
 from sys import argv
 
-
 def main():
-    """Main function"""
-    user_Id = argv[1]
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos/{}".format(user_Id), verify=false).json()
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_Id), verify=false).json()
-    
+    endpoint = "https://jsonplaceholder.typicode.com"
+    userId = argv[1]
+    user = requests.get(endpoint + "users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get(endpoint + "todos?userId={}".
+                        format(userId), verify=False).json()
     completed_tasks = []
     for task in todo:
         if task.get('completed') is True:
@@ -17,6 +19,6 @@ def main():
     print("Employee {} is done with tasks({}/{}):".
           format(user.get('name'), len(completed_tasks), len(todo)))
     print("\n".join("\t {}".format(task) for task in completed_tasks))
-
+    
 if __name__ == '__main__':
     main()
